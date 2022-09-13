2b5c4a265a99a7b1efc69b8f28d67b1bb56d7c12
James <entreur@gmail.com>
2022-09-13 19:22:31 +0400

    Set up general structure for player.

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //In this version of the game, the player won't die immediately after being hit by the wizard.
 func main() {
-	fmt.Println("Hello World")
+	var pName string
+
+	fmt.Print("Welcome Wizard, insert your name:")
+	fmt.Scan(&pName)
+	fmt.Printf("Your name is %s? What a stupid name.\n", pName)
 }
+
+//Struct is basically a class. It needs to be initialized via a method.
+type Player struct {
+	Name   string
+	Level  int
+	Health int
+}
+
+func generatePlayer(name string) *Player {
+	p := Player{Name: name}
+	return &p
+}
+
+/*func generatePlayer(name string) *Player {
+	p := Player{Name: name}
+	return &p
+}*/
