e0ae61ec8cf2d0743b5ba7a3449efd15dfa963c2
James <entreur@gmail.com>
2022-09-14 12:27:35 +0400

    Created structs for player and wizard

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 //In this version of the game, the player won't die immediately after being hit by the wizard.
 func main() {
 	var pName string
-
 	fmt.Print("Welcome Wizard, insert your name:")
 	fmt.Scan(&pName)
 	fmt.Printf("Your name is %s? What a stupid name.\n", pName)
+	fmt.Printf("You are level %d wizard.", generatePlayer(pName).Level)
 }
 
 //Struct is basically a class. It needs to be initialized via a method.
@@ -19,13 +20,25 @@ type Player struct {
 	Level  int
 	Health int
 }
-
-func generatePlayer(name string) *Player {
-	p := Player{Name: name}
-	return &p
+type Wizard struct {
+	Name   string
+	Level  int
+	Health int
 }
 
-/*func generatePlayer(name string) *Player {
-	p := Player{Name: name}
+func generatePlayer(name string) *Player {
+	var level int = rand.Intn(10-1) + 1
+	p := Player{Name: name, Level: level, Health: 10}
 	return &p
-}*/
+}
+func generateWizard() *Wizard {
+	var wizardNames = []string{"Homer",
+		"Kwon",
+		"Riah",
+		"Uluq-UlDudush",
+		"Ravenlord"}
+	var wizardName string = wizardNames[rand.Intn(10-1)+1]
+	var level int = rand.Intn(10-1) + 1
+	w := Wizard{Name: wizardName, Level: level, Health: 10}
+	return &w
+}
