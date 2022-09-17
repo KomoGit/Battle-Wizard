54f39f79e598f923a6100cc34eba278dd9022e6d
James <entreur@gmail.com>
2022-09-17 10:51:09 +0400

    Added input interpreter to code.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 //In this version of the game, the player won't die immediately after being hit by the wizard.
@@ -11,7 +13,6 @@ func main() {
 	fmt.Print("Welcome Wizard, insert your name:")
 	fmt.Scan(&pName)
 	fmt.Printf("Your name is %s? What a stupid name. You are a level %d wizard\n", pName, generatePlayer(pName).Level)
-	fmt.Printf("A wild wizard named %s appeared. He is level %d\n", generateWizard().Name, generateWizard().Level)
 	gameLoop(10)
 }
 
@@ -29,7 +30,29 @@ type Wizard struct {
 
 func gameLoop(playerHealth int) {
 	for playerHealth >= 0 {
-
+		fmt.Printf("A wild wizard named %s appeared. He is level %d\n", generateWizard().Name, generateWizard().Level)
+		fmt.Println("Press A to Attack\nPress B to Run Away")
+		reader := bufio.NewReader(os.Stdin)
+		char, _, err := reader.ReadRune()
+		if err != nil {
+			fmt.Print(err)
+		}
+		keyCode := char
+		inputInterpreter(int(keyCode))
+	}
+}
+func inputInterpreter(char int) {
+	switch char {
+	case 65:
+		fmt.Println("Pressed A")
+	case 97:
+		fmt.Println("Pressed a")
+	case 66:
+		fmt.Println("Pressed B")
+	case 98:
+		fmt.Println("Pressed b")
+	default:
+		return
 	}
 }
 func generatePlayer(name string) *Player {
