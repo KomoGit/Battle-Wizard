caccc042dbcec208972ce33ce621c57f0d5ed21e
James <entreur@gmail.com>
2023-03-08 11:54:55 +0400

    Dart files added

---
+++ bin/battle_wizard.dart
@@ -0,0 +1,5 @@
+import 'package:battle_wizard/battle_wizard.dart' as battle_wizard;
+
+void main(List<String> arguments) {
+  print('Hello world: ${battle_wizard.calculate()}!');
+}
---
+++ test/battle_wizard_test.dart
@@ -0,0 +1,8 @@
+import 'package:battle_wizard/battle_wizard.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
