6f4d95f62b78f54c272be0557be6ca1f6deb7dea
E.A Nasir <example@example.com>
2023-06-09 14:49:46 +0400

    Generating wizard outside of class.

--- wizard.py
+++ wizard.py
@@ -1,27 +1,11 @@
-import random as rnd
-
 class Wizard:
-    def __init__():
-        Name = GenerateWizardName() 
-        Level = GenerateWizardLevel()
+
+    # How class constructors are created. Use __init__():
+    def __init__(self,name,level):
+        self.Name = name #GenerateWizardName() 
+        self.Level = level #GenerateWizardLevel()
+
+     
         
-def GenerateWizardName():
-        wizard_name_index = rnd.randrange(0,len(possible_wizard_names))
-        return possible_wizard_names[wizard_name_index]
-
-def GenerateWizardLevel():
-        return rnd.randrange(0,11)
-
-possible_wizard_names = ["Uluq Ul Dudush", 
-                             "Lost little boy", 
-                             "Ravenlord", 
-                             "Johnny Sins", 
-                             "Xi Jingping",
-                             "Autismo",
-                             "Shrek"]
-
-Name = ""
-Level = 0
-
 
     
