703c749a1bc757cbf11a9c75492b759fc1790c36
James <entreur@gmail.com>
2023-06-09 22:56:54 +0400

    Trying to fix

---
+++ PlayerClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Battle_Wizard
+{
+    class PlayerClass
+    {
+        //public int playerRoll;
+        public string Player = Console.ReadLine();
+        public void playerName()
+        {
+            switch (this.Player) //These are the names for player. Only serve easter egg purposes, nothing else.
+            {  //This class will also hold level system, experience, and play exclusive spells.
+                case "Elnur":
+                    Console.WriteLine("Welcome Developer! " + "\nPress any button to begin.");
+                    Console.ReadKey();
+                    break;
+                case "Snake":
+                    Console.WriteLine("Kept you waiting huh? " + "\nPress any button to begin.");
+                    Console.ReadKey();
+                    break;
+                case "Raiden":
+                    Console.WriteLine("It's time for Jack, to let er' rip! " + "\nPress any button to begin.");
+                    Console.ReadKey();
+                    break;
+                case "Developer":
+                    Console.WriteLine("Developer Mode On.");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Your name is? " + Player + " What a stupid name." + "\nPress any button to begin.");
+                    Console.ReadKey();
+                    break;
+            }
+        }       
+    }
+}
