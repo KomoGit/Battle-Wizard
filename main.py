bfcff6a6b257b676759305fd3351819227a257b2
E.A Nasir <example@example.com>
2023-06-09 15:06:21 +0400

    Remove unusued code.

--- main.py
+++ main.py
@@ -8,17 +8,13 @@ def GameLoop(gameActive):
     while(gameActive):
         print("Press A to Attack || Press B to Run Away")
         userInput = input().upper
-        # ValidateUserInput(userInput)
 
-# def ValidateUserInput(userInput):
-#    if(userInput == "A"):
-#        return  
-   
+
 playerName = input("Greetings Wizard, Insert your name: ")
 playerLevel = GenerateRandomInt()
 print(f"Your name is {playerName}? What a stupid name! You are a level {playerLevel} wizard.")
 input("Press any key to being...")
 gameLoopActive = True
 userInput = input()
-GameLoop(gameLoopActive,userInput)
+GameLoop(gameLoopActive)
 
