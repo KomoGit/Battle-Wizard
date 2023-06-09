88de5c9c78452156451ed626234d900d5dcf71e5
James <entreur@gmail.com>
2023-06-09 21:20:58 +0400

    "Wizards are now being generated"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random as rnd
+import wizard as w
 import wizard_generator as wiz_gen
 
 
@@ -9,11 +10,13 @@ def generate_random_int():
 
 def game_loop(game_active):
     while game_active:
+        enemy = w.Wizard(wiz_gen.generate_wizard_name(), generate_random_int())
+        print(f"A wild wizard named {enemy.Name} appeared. Wizard is level : {enemy.Level}")
         print("Press A to Attack || Press B to Run Away")
-        print(wiz_gen.generate_wizard_name())
-        input("Generate another")
+        input()
 
 
+# This thing is only used once at the start of the game.
 def player_name_prompt():
     name_prompt_active = True
     while name_prompt_active:
@@ -30,9 +33,5 @@ def player_name_prompt():
 playerName = player_name_prompt()
 playerLevel = generate_random_int()
 print(f"Your name is {playerName}? What a stupid name! You are a level {playerLevel} wizard.")
-input("Press any key to being...")
 gameLoopActive = True
-userInput = input()
 game_loop(gameLoopActive)
-
-
