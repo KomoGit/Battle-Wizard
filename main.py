81db5561ec3e42daacb7f90b938e04b41c1e3532
James <entreur@gmail.com>
2023-06-10 18:31:21 +0400

    Core of the game now works. Needs a few more features.

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ def generate_random_int():
     return rnd.randrange(1, 11)
 
 
+def interpret_user_input(inpt, wizard):
+    if inpt.upper() == "A":
+        if w.battle(wizard.Level, player_level=playerLevel):
+            print(f"You killed {wizard.Name} with a spell.")
+        else:
+            print(f"{wizard.Name} killed you with a spell.")
+            exit(0)
+    elif inpt.upper() == "B":
+        print("Player Pressed B")
+    elif inpt.upper() == "C":
+        print(f"You are level - {playerLevel}, While the enemy is {wizard.Level}")
+    else:
+        print("Invalid input.")
+
+
 def game_loop(game_active):
     while game_active:
         enemy = w.Wizard(wiz_gen.generate_wizard_name(), generate_random_int())
         print(f"A wild wizard named {enemy.Name} appeared. Wizard is level : {enemy.Level}")
-        print("Press A to Attack || Press B to Run Away")
-        input()
+        print("Press A to Attack || Press B to Run Away || Press C to Show Stats")
+        interpret_user_input(input(), enemy)
 
 
 # This thing is only used once at the start of the game.
